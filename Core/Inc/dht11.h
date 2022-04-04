f1392740762eccc73193e8aad28d0bd4d6a8138f
Umur <umurdincer3@gmail.com>
2022-04-04 18:58:15 +0300

    work on dht11 APIs

--- Core/Inc/dht11.h
+++ Core/Inc/dht11.h
@@ -8,6 +8,24 @@
 #ifndef INC_DHT11_H_
 #define INC_DHT11_H_
 
+#include"main.h"
+
+#define INPUT	0
+#define OUTPUT	1
+
+typedef struct{
+
+	GPIO_TypeDef* GPIOx; // configure GPIO port. e.g. GPIOD
+	uint16_t Sensor_Pin; //	configure Pin
+	uint8_t temperature;
+	uint8_t humidity;
+
+}DHT11_HandleTypeDef;
+
+
+void DHT11_Init(DHT11_HandleTypeDef *dht11);
+void set_DHT11_input_output(DHT11_HandleTypeDef *dht11 , uint8_t In_Out);
+uint8_t read_DHT11(DHT11_HandleTypeDef*);
 
 
 #endif /* INC_DHT11_H_ */
--- Core/Src/dht11.c
+++ Core/Src/dht11.c
@@ -5,4 +5,29 @@
  *      Author: ASUS
  */
 
+#include"dht11.h"
 
+GPIO_InitTypeDef gpio_dht11;
+
+void DHT11_Init(DHT11_HandleTypeDef *dht11)
+{
+	gpio_dht11.Pin = dht11->Sensor_Pin;
+	gpio_dht11.Pull = GPIO_NOPULL;
+	gpio_dht11.Speed = GPIO_SPEED_FAST;
+
+	HAL_GPIO_Init(dht11->GPIOx, &gpio_dht11);
+}
+
+void set_DHT11_input_output(DHT11_HandleTypeDef *dht11 , uint8_t In_Out)
+{
+	if(In_Out == INPUT){
+		gpio_dht11.Mode = GPIO_MODE_INPUT;
+		HAL_GPIO_Init(dht11->GPIOx, &gpio_dht11);
+	}
+	else if(In_Out == OUTPUT){
+		gpio_dht11.Mode = GPIO_MODE_OUTPUT_PP;
+		HAL_GPIO_Init(dht11->GPIOx, &gpio_dht11);
+	}
+
+
+}
